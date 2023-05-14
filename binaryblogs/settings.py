ed1e94dd373cd2cee9e81ca98be9b88f2a392138
santanu2310 <s.m.23102004@gmail.com>
2023-05-14 08:09:22 +0530

    making some shanges

--- binaryblogs/settings.py
+++ binaryblogs/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEIDA_URL = 'images/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'images/')
 
 # CKEDITOR_UPLOAD_PATH = 'static/images/blog_image'
 
